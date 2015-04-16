58725d083304e01e083c67eb7f15d5894d7c0dc0
CryoFlar3 <ndukes@computermentors.org>
2015-04-16 10:33:08 -0400

    Added a few lines
    added two variables. Thats it.

--- app/src/main/java/computermentors/org/childdevelopment/MilestoneActivity.java
+++ app/src/main/java/computermentors/org/childdevelopment/MilestoneActivity.java
@@ -8,10 +8,16 @@ import android.view.MenuItem;
 
 public class MilestoneActivity extends ActionBarActivity {
 
+    private String mDob;
+    private String mEarlyBirth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_milestone);
+
+        mDob = getIntent().getStringExtra(getString(R.string.dob));
+        mEarlyBirth = getIntent().getStringExtra(getString(R.string.early));
 
         //loadPage();
     }
